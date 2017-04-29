4c1f5ce267aa8ed5b3c1a294ca8c88f409a9df6e
Pavel Ponkrashov <mr_Panek@mail.ru>
2017-04-30 01:02:39 +0300

    Update Architect Converter

--- Controllers/supportFunction/Converter.cs
+++ Controllers/supportFunction/Converter.cs
@@ -5,7 +5,18 @@ using System.Web;
 
 namespace VK_Analyze.Controllers.supportFunction
 {
-    public class Converter
+    public static class Converter
     {
+        public static IEnumerable<CityInfo> ToCityInfoCollection(Dictionary<string,int> citys)
+        {
+            List<CityInfo> cityInfoCollection = new List<CityInfo>();
+
+            foreach (string key in citys.Keys)
+            {
+                cityInfoCollection.Add(new CityInfo(key, citys[key]));
+            }
+
+            return cityInfoCollection;
+        }
     }
 }
