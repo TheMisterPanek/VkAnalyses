b40c77603e2e7659a9c8b73bf9296d83f29c4fc3
Pavel Ponkrashov <mr_Panek@mail.ru>
2017-04-30 00:17:53 +0300

    add Converter, create ToCityInfoConverter

---
+++ Controllers/supportFunction/CityInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VK_Analyze.Controllers.supportFunction
+{
+    public class CityInfo
+    {
+        public string City { get; set; }
+        public int CountFriends { get; set; }
+        public CityInfo(string City,int CountFriends)
+        {
+            this.City = City;
+            this.CountFriends = CountFriends;
+        }
+    }
+}
---
+++ Controllers/supportFunction/Converter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VK_Analyze.Controllers.supportFunction
+{
+    public class Converter
+    {
+    }
+}
