626af47f40b87464a8d9cb22bee82e15fd795463
Pavel Ponkrashov <mr_Panek@mail.ru>
2017-04-30 01:13:54 +0300

    temp work Friends View-Controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,55 +14,56 @@ namespace VK_Analyze.Controllers
     public class HomeController : Controller
     {
          VkApi vk;
-
         VkNet.Model.User userInfo;
+       
 
         public HomeController()
         {
-            CookieTokenWorker cookie = new CookieTokenWorker(new HttpCookie("VkAnalyses"));
+            CookieTokenWorker cookie = new CookieTokenWorker("VkAnalyses");
+            vk = cookie.GetVkApiFromCookie();
         }
 
 
         public ActionResult Index()
         {
-           
+        
             return View();
         }
 
-        public void Friends(params string[] args)
-        {
-           
-        }
+      
 
         [HttpGet]
         public ActionResult Friends(string userID = "0")
         {
-            ViewBag.Text = "Друзья";
-            ViewBag.Users = VkFriends.GetUsers(vk);
+            HttpCookie cookieReq = Request.Cookies["VkAnalyses"];
+            CookieTokenWorker cookie = new CookieTokenWorker(cookieReq);
+            Dictionary<string, int> Dict = VkFriends.AnalyseFriends(cookie.GetVkApiFromCookie());
+
+            ViewBag.Citys = supportFunction.Converter.ToCityInfoCollection(Dict).ToList();
             return View();
         }
 
 
 
-        [HttpPost]
-        public ActionResult Friends(UserFriendsView model)
-        {
-            ViewBag.User = userInfo;
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    CookieTokenWorker cookie = new CookieTokenWorker(new HttpCookie("VkAnalyses"));
-                    VkApi vkApi = cookie.GetVkApiFromCookie();
-                    ViewBag.Users = VkFriends.GetUsers(vkApi, model.UserID);
-                }
-                catch(Exception)
-                {
-                    ViewBag.Users = new VkNet.Model.User[0];
-                }
-            }
-            return View();
-        }
+        //[HttpPost]
+        //public ActionResult Friends(UserFriendsView model)
+        //{
+        //    ViewBag.User = userInfo;
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            CookieTokenWorker cookie = new CookieTokenWorker(new HttpCookie("VkAnalyses"));
+        //            VkApi vkApi = cookie.GetVkApiFromCookie();
+        //            ViewBag.Users = VkFriends.GetUsers(vkApi);
+        //        }
+        //        catch(Exception)
+        //        {
+        //            ViewBag.Users = new VkNet.Model.User[0];
+        //        }
+        //    }
+        //    return View();
+        //}
 
 
         public ActionResult About()
